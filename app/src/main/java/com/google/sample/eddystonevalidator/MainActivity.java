78a2d44914abf40f73ad76284c769b3a7690b288
Ivo3 <ivobaarkit@gmail.com>
2016-06-11 16:09:46 +0200

    Added Floating Bubble (warning float thing)

--- app/src/main/java/com/google/sample/eddystonevalidator/MainActivity.java
+++ app/src/main/java/com/google/sample/eddystonevalidator/MainActivity.java
@@ -19,6 +19,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * MainActivity for the Eddystone Validator sample app.
@@ -29,6 +31,31 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Bundle bundle = getIntent().getExtras();
+
+    /*
+    if(bundle != null && bundle.getString("LAUNCH").equals("YES")) {
+      startService(new Intent(MainActivity.this, FlyBitch.class));
+    }
+    */
+    Button launch = (Button)findViewById(R.id.button1);
+    launch.setOnClickListener(new View.OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        startService(new Intent(MainActivity.this, FlyBitch.class));
+      }
+    });
+
+    Button stop = (Button)findViewById(R.id.button2);
+    stop.setOnClickListener(new View.OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        stopService(new Intent(MainActivity.this, FlyBitch.class));
+      }
+    });
   }
 
   @Override
