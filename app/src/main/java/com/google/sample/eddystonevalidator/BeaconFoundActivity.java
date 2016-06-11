1459cadf7b12e5d0c840af06aefcfd1cf4243122
Anton Huck <antonhuck@gmail.com>
2016-06-11 19:26:48 +0200

    added Beacon listener

---
+++ app/src/main/java/com/google/sample/eddystonevalidator/BeaconFoundActivity.java
@@ -0,0 +1,18 @@
+package com.google.sample.eddystonevalidator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+
+/**
+ * Created by anton on 6/11/2016.
+ */
+public class BeaconFoundActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        Log.e("LOA", "activity started");
+    }
+}
